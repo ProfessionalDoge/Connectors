469e37f9e439ef084bb18c0657f88b8e7b8a06a4
ProfessionalDoge <90437229+ProfessionalDoge@users.noreply.github.com>
2022-03-28 11:50:51 +0800

    Update rblx3008connectors.lua
    newer script

--- rblx3008connectors.lua
+++ rblx3008connectors.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Daniel-Herron/Wulphram/main/SCP3008.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ProfessionalDoge/scripts/main/scp3008con2.lua",true))()
