9c940049a184b75c7346dc5d01d2831a94dc3484
ProfessionalDoge <90437229+ProfessionalDoge@users.noreply.github.com>
2022-03-28 12:11:44 +0800

    Update drvempcnctr.lua

--- drvempcnctr.lua
+++ drvempcnctr.lua
@@ -1,5 +1,6 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/zTsXJM3K"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/GodXNation/f16e964be44b219ee07de514bcb5b63d/raw/4a92a27962acf944e4f40e9bc0049485e47b5166/midnightracingMAINHUB", true))()
 
-print("To disable script u need to rejoin")
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "KEY", Text = "artanis"})
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "KEY", Text = "artanis"})
 
 end
