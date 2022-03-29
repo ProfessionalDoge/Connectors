5784a51da353c23ca72b2d822e6923508001dba3
ProfessionalDoge <90437229+ProfessionalDoge@users.noreply.github.com>
2022-03-29 08:22:24 +0800

    Update drvempcnctr.lua

--- drvempcnctr.lua
+++ drvempcnctr.lua
@@ -1,5 +1,4 @@
 loadstring(game:HttpGet("https://gist.githubusercontent.com/GodXNation/f16e964be44b219ee07de514bcb5b63d/raw/4a92a27962acf944e4f40e9bc0049485e47b5166/midnightracingMAINHUB", true))()
-then
 
 game:GetService("StarterGui"):SetCore("SendNotification", {Title = "KEY", Text = "artanis"})
 game:GetService("StarterGui"):SetCore("SendNotification", {Title = "KEY", Text = "artanis"})
